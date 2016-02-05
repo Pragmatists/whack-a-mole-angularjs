287947bbfc8a8b90e3bfcc9b18e1827ee28c9e65
Pawel Barszcz <pawelbarszcz@gmail.com>
2016-02-05 12:24:08 +0100

    Mole is now a button, not an "img"

--- src/app/mole/mole.directive.spec.js
+++ src/app/mole/mole.directive.spec.js
@@ -19,7 +19,7 @@ describe('"mole" directive', function () {
 
     // then
     expect(element.find('.mole')).toExist();
-    expect(element.find('.mole__image')).toExist();
+    expect(element.find('.mole__button')).toExist();
   });
 
   it('is not visible by default', function () {
@@ -50,7 +50,7 @@ describe('"mole" directive', function () {
     wait(TIME_TO_TOGGLE_MOLE_VISIBILITY);
 
     // when
-    element.find('.mole__image').trigger('click');
+    element.find('.mole__button').trigger('click');
 
     // then
     expect(element.find('.mole')).toHaveClass('mole--hidden');
@@ -67,7 +67,7 @@ describe('"mole" directive', function () {
     wait(TIME_TO_TOGGLE_MOLE_VISIBILITY);
 
     // when
-    element.find('.mole__image').trigger('click');
+    element.find('.mole__button').trigger('click');
 
     // then
     expect(callbackWasCalled).toBe(true);
